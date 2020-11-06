06b811d48c4e704ae5c1e4ae64d88f186234a146
Rask971 <73994946+Rask971@users.noreply.github.com>
2020-11-06 16:08:56 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,11 @@
+
+/* Объяви переменные a, b типа int и переменную s типа String.
+Сразу же в строке объявления присвой им любые разные значения. */
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 8;
+        String s = "Alex";
+    }
+}
