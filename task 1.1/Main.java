6b4ac9bc29f4901604e6d15a1a77fa439adceb6f
Rask971 <73994946+Rask971@users.noreply.github.com>
2020-11-06 15:45:53 +0300

    Update Main.java

--- task 1.1/Main.java
+++ task 1.1/Main.java
@@ -1,1 +1,8 @@
-
+public class Main {
+    public static void main(String[] args) {
+        String name = "Александр";
+        int age = 34;
+        String city = "Москва";
+        System.out.println(name+" "+age+" "+city);
+    }
+}
