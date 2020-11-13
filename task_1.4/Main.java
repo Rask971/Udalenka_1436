3509bf6af23b242f6e6ff482774b03363c456ff3
Rask971 <73994946+Rask971@users.noreply.github.com>
2020-11-13 13:05:58 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -5,6 +5,6 @@
 public class Main {
     public static void main(String[] args) {
         int number = 25;
-        System.out.println(number);
+        System.out.println(number*number);
     }
 }
